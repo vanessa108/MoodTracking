e46a33dca44c16fa8aa467254f2bd32682e5170c
Vanessa <vanessa.li0810@gmail.com>
2019-04-03 10:09:00 +0200

    fragments!

--- app/src/main/java/com/example/moodtracking/MainActivity.java
+++ app/src/main/java/com/example/moodtracking/MainActivity.java
@@ -4,46 +4,64 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_mood:
-                    Intent intent1 = new Intent(MainActivity.this, MainActivity.class);
-                    startActivity(intent1);
-                    return true;
-                case R.id.navigation_calendar:
-                    Intent intent2 = new Intent(MainActivity.this, Calendar.class);
-                    startActivity(intent2);
-                    return true;
-                case R.id.navigation_stats:
-                    Intent intent3 = new Intent(MainActivity.this, Stats.class);
-                    startActivity(intent3);
-                    return true;
-                case R.id.navigation_settings:
-                    Intent intent4 = new Intent(MainActivity.this, Settings.class);
-                    startActivity(intent4);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_calendar:
+                fragment = new CalendarFragment();
+                break;
+
+            case R.id.navigation_stats:
+                fragment = new StatsFragment();
+                break;
+
+            case R.id.navigation_settings:
+                fragment = new SettingsFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+
 }
+
+
