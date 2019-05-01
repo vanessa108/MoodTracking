16ce150889dc385839c59b01342e63f92aa9e50e
Daniela Frolich <danfrol@chalmers.student.se>
2019-05-01 14:23:25 +0200

    xaxis dates lables

--- app/src/main/java/com/example/moodtracking/StatsBarChartFragment.java
+++ app/src/main/java/com/example/moodtracking/StatsBarChartFragment.java
@@ -42,6 +42,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -67,9 +68,16 @@ public class StatsBarChartFragment extends Fragment {
         final RelativeLayout relativeLayout = (RelativeLayout) inflater.inflate(R.layout.fragment_stats_bar_chart, container, false);
 
         TextView date_2 = (TextView) relativeLayout.findViewById(R.id.date_2);
+        TextView textViewSleep_2 = (TextView) relativeLayout.findViewById(R.id.textViewSleep_2);
         TextView tv_month = (TextView) relativeLayout.findViewById(R.id.textViewMonth);
+        TextView textViewExercise_2 = (TextView) relativeLayout.findViewById(R.id.textViewExercise_2);
         BarChart barChart_2 = (BarChart) relativeLayout.findViewById(R.id.barchart_2);
         Button pie_chart_button = (Button) relativeLayout.findViewById(R.id.pieChartButton);
+
+        TextView startSleep_2 = (TextView) relativeLayout.findViewById(R.id.StartBedTime_2);
+        List<TextView> sSleep_2 = Arrays.asList(startSleep_2);
+        TextView endSleep_2 = (TextView) relativeLayout.findViewById(R.id.EndBedTime_2);
+        ProgressBar proBar_2 = (ProgressBar) relativeLayout.findViewById(R.id.sleepCircle_2);
 
         String date = new SimpleDateFormat("EEE d MMM yyyy", Locale.getDefault()).format(new Date());
         date_2.setText("" + date);
@@ -96,40 +104,87 @@ public class StatsBarChartFragment extends Fragment {
         ArrayList<BarEntry> barEntriesSleep = new ArrayList<>();
         ArrayList<BarEntry> barEntriesExercise = new ArrayList<>();
 
+
+        /*Read data*/
+        InputStream exp = getResources().openRawResource(R.raw.export);
+        InputStream mod = getResources().openRawResource(R.raw.mooddata);
+        HealthData hd = new HealthData(exp,mod);
+        List<extData> sd = null;
+        List<extData> md = null;
+        List<extData> ad = null;
+
+        try {
+            int lastdays = 31;
+            //get the data from the "lasdays" Order of data is today-n -> reverse data for bars after adding it to the barchart
+            sd = hd.getSleepData(lastdays);
+            md = hd.getMoodData(lastdays);
+            ad = hd.getStepData(lastdays);
+
+            for(int i = 0;i<lastdays;i++){
+
+                //activityData
+                long actMin = (long)ad.get(i).getValue();
+                barEntriesExercise.add(new BarEntry(i, Float.valueOf(actMin)));
+
+
+                //SleepData
+                Date temp_start = (Date)sd.get(i).getStartDate();
+                Date temp_end   = (Date)sd.get(i).getEndDate();
+                long sleep_min = (long)sd.get(i).getValue();
+                barEntriesSleep.add(new BarEntry(i, Float.valueOf(sleep_min)));
+
+                //Sleep circle moon
+                setSleepCycleView(temp_start, temp_end, startSleep_2, endSleep_2, proBar_2);
+                if( i==0){
+                    setSleepCycleView(temp_start,temp_end,startSleep_2,endSleep_2,proBar_2);
+                    textViewSleep_2.setText(getTimeFromMin(sleep_min));
+                }
+
+                //TODO mood data
+
+
+
+            }
+
+            long actMin2 = (long)ad.get(0).getValue();
+            String actMinStr = Long.toString(actMin2);
+            textViewExercise_2.setText(actMinStr + " min");
+
+            Collections.reverse(barEntriesSleep);
+            Collections.reverse(barEntriesExercise);
+
+
+
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+
         //BarDataSets
         BarDataSet barDataSetExercise = new BarDataSet(barEntriesExercise, "Exercise");
-
-        barEntriesExercise.add(new BarEntry(0, 40f));
-        barEntriesExercise.add(new BarEntry(1, 30f));
-        barEntriesExercise.add(new BarEntry(2, 20f));
-        barEntriesExercise.add(new BarEntry(3, 46f));
-        barEntriesExercise.add(new BarEntry(4, 40f));
-        barEntriesExercise.add(new BarEntry(5, 30f));
-        barEntriesExercise.add(new BarEntry(6, 20f));
-        barEntriesExercise.add(new BarEntry(7, 46f));
-
-        barEntriesSleep.add(new BarEntry(0, 600f));
-        barEntriesSleep.add(new BarEntry(1, 500f));
-        barEntriesSleep.add(new BarEntry(2, 400f));
-        barEntriesSleep.add(new BarEntry(3, 450f));
-        barEntriesSleep.add(new BarEntry(4, 600f));
-        barEntriesSleep.add(new BarEntry(5, 500f));
-        barEntriesSleep.add(new BarEntry(6, 400f));
-        barEntriesSleep.add(new BarEntry(7, 450f));
-
         barDataSetExercise.setColors(Color.rgb(242, 162, 162));
+
         BarDataSet barDataSetSleep = new BarDataSet(barEntriesSleep, "Sleep");
         barDataSetSleep.setColors(Color.rgb(136, 139, 221));
+
+
         barDataSetSleep.setValueFormatter(new SleepValueFormatter());
+        barDataSetExercise.setValueFormatter(new SleepValueFormatter());
 
         barDataSetExercise.setValueTextSize(14f);
         barDataSetSleep.setValueTextSize(14f);
 
         BarData data = new BarData(barDataSetExercise, barDataSetSleep);
 
-        float groupSpace = 0.5f;
-        float barSpace = 0.13f;
-        float barWidth = 0.12f;
+        float barSpace = 0.16f;
+        float barWidth = 0.15f;
+        float groupSpace = calcGroupSpace(2, barSpace, barWidth);
 
         barChart_2.setData(data);
         data.setBarWidth(barWidth);
@@ -140,13 +195,13 @@ public class StatsBarChartFragment extends Fragment {
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setGranularity(1);
         barChart_2.getXAxis().setGranularityEnabled(true);
-        xAxis.setDrawGridLines(false);
-        xAxis.setAxisMinimum(1f);
-        xAxis.setAxisMaximum(31);
+        xAxis.setDrawGridLines(true);
+        xAxis.setAxisMinimum(1);
+        xAxis.setAxisMaximum(32);
         xAxis.setValueFormatter(new dateFormatter());
         xAxis.setTextSize(16f);
         barChart_2.setExtraOffsets(0,0,20,12);
-
+        xAxis.setCenterAxisLabels(true);
 
         //Y-axis
         YAxis yAxisLeft = barChart_2.getAxisLeft();
@@ -158,9 +213,8 @@ public class StatsBarChartFragment extends Fragment {
         //Scrolling
         barChart_2.setDragEnabled(true);
         barChart_2.setVisibleXRangeMaximum(5);
-        barChart_2.moveViewToX(31); //TODO set to today
+        barChart_2.moveViewToX(31);
         barChart_2.invalidate();
-        //barChart_2.get
     return relativeLayout;
     }
 
@@ -191,13 +245,71 @@ public class StatsBarChartFragment extends Fragment {
         @Override
         public String getFormattedValue(float value, AxisBase axis) {
             Calendar cal = Calendar.getInstance();
-            DateFormat dateFormat = new SimpleDateFormat("d");
-            int val;
-            val = Math.round(value);
-            cal.add(Calendar.DATE, val);
+
+            String pattern = "d";
+            int val= Math.round(value);
+            cal.add(Calendar.DATE, val - 31);
+            if(val - 31 == 0){
+                pattern = "d/M";
+            }
+            DateFormat dateFormat = new SimpleDateFormat(pattern);
             String date = dateFormat.format(cal.getTime());
 
             return date;
         }
     }
+
+    private String getTime(Date in){
+        if(in!= null){
+            String formattedDate = new SimpleDateFormat("HH:mm").format(in);
+            return formattedDate;
+        }
+        else{
+            return "00:00";
+        }
+    }
+
+    private int calcProgress(Date start,Date end){
+        long diff = end.getTime() - start.getTime();
+        return (int)diff/(12*60*60*10);
+    }
+
+    private int calcStartAngleProgressBar(Date start){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(start);
+        int hours = calendar.get(Calendar.HOUR_OF_DAY);
+        int minutes = calendar.get(Calendar.MINUTE);
+        int startOfProgressBar = -90;
+        int angle = ((60*hours+minutes)/2)+startOfProgressBar;
+        return angle;
+    }
+
+    private void setSleepCycleView(Date start,Date end,TextView startText,TextView endText,ProgressBar probar) {
+        if ((start != null) && (end != null)) {
+            startText.setText(getTime(start));
+            endText.setText(getTime(end));
+            probar.setProgress(calcProgress(start, end));
+            probar.setRotation(calcStartAngleProgressBar(start));
+        } else {
+            startText.setText("");
+            endText.setText("");
+            probar.setProgress(0);
+            probar.setRotation(0);
+        }
+    }
+
+    private String getTimeFromMin(long min){
+        long diffHours =min/ 60;
+        long diffMinutes = min%60;
+
+        return diffHours + "h "+ diffMinutes +"m";
+    }
+
+    private float calcGroupSpace(float n, float barSpace, float barWidth){
+        float groupSpace = 1 - (barSpace + barWidth) * n;
+
+        return groupSpace;
+    }
+
+
 }
